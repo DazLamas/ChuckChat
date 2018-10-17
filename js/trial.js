5b3a47997dbeb63c9bda54380625c4eced0aa74d
DazLamas <sarah.d.lamas@gmail.com>
2018-10-17 12:13:48 +0200

    Reset branch state. Remove Joke-Contest files and and Chuck-Therapy ones

---
+++ js/trial.js
@@ -0,0 +1,21 @@
+//
+//
+// function getChuckAnswer() {
+//
+//    $.ajax({
+//       url: 'http://store.apicultur.io/api/refranesaleatorios/1.0.0',
+//       dataType: 'application/json',
+//       complete: function(data){
+//           placeNewComment('Chuck Norris', JSON.parse(data.responseText).value, 'chucknorris.jpg');
+//           scrollToLastTextBox();
+//       }
+//   })
+// };
+// URL for expressions: http://store.apicultur.io/api/refranesaleatorios/1.0.0
+// var request = new XMLHttpRequest();
+// request.open("GET", "", false);
+// request.send();
+// var xml = request.responseXML;
+// var users = xml.getElementsByTagName("user");
+//
+//
