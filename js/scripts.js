89bb08447e3680cee93675a216e382369772879b
DazLamas <sarah.d.lamas@gmail.com>
2018-11-02 13:22:18 +0100

    Replace jQuery with Vanilla JS III. Ajax request

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-(function ($, window, document, undefined) {
+(function (window, document, undefined) {
 
   var new_comment_data           = {username: "", text: ""};
   var new_comment_send_button    = document.getElementById('js-send-btn');
@@ -16,10 +16,10 @@
     for (var i = 0; i < form_inputs.length; i++) {
       new_comment_data[form_inputs[i].name] = form_inputs[i].value;
     };
-    createAndPlaceNewComment(new_comment_data.username, new_comment_data.text);
+    createAndPlaceNewMessage(new_comment_data.username, new_comment_data.text);
   };
 
-  function createAndPlaceNewComment(username, text, avatar_url) {
+  function createAndPlaceNewMessage(username, text, avatar_url) {
 
     avatar = avatar_url || 'abuela-avatar-bg-x2.png';
 
@@ -35,10 +35,8 @@
     new_comment_form_container.insertAdjacentHTML('beforebegin', string);
 
     scrollToLastTextBox();
-
     insertScore(username, document.getElementsByClassName('js-place-score-here')[0]);
-
-    if(username !== 'Chuck Norris'){//Prevent infinitive bucle with Chuck Norris' answers
+    if(username !== 'Chuck Norris'){
       getChuckAnswer();
     };
 
@@ -46,20 +44,30 @@
 
   function getChuckAnswer() {
 
-     $.ajax({
-        url: 'https://api.chucknorris.io/jokes/random',
-        dataType: 'application/json',
-        complete: function(data){
-            createAndPlaceNewComment(
-                            'Chuck Norris',
-                            JSON.parse(data.responseText).value,
-                            'chucknorris.jpg'
-                          )
-        }
-    })
-  };
+      var xmlhttp = new XMLHttpRequest();
 
-  // Init method:
+      xmlhttp.onreadystatechange = function() {
+          if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+             if (xmlhttp.status == 200) {
+                 createAndPlaceNewMessage(
+                                 'Chuck Norris',
+                                 JSON.parse(xmlhttp.responseText).value,
+                                 'chucknorris.jpg'
+                               );
+             }
+             else if (xmlhttp.status == 400) {
+                showError('Try it again or leave');
+             }
+             else {
+                showError('Send an angry message to the webmaster, please');
+             }
+          }
+      };
+      xmlhttp.open("GET", "https://api.chucknorris.io/jokes/random", true);
+      xmlhttp.send();
+    };
+
+  /* Init method: */
   new_comment_send_button.addEventListener("click", function(e) {
 
     resetAll();
@@ -73,4 +81,4 @@
   }, false);
 
 
-})(jQuery, window, document);
+})(window, document);
